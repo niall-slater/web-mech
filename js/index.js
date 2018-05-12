1a541bf9732145554e33b53c53cf1374d7d6ca0a
NigeyNige <niallslater@gmail.com>
2018-05-12 14:50:07 +0100

    Added basic map generation

--- js/index.js
+++ js/index.js
@@ -15,12 +15,13 @@ var mech = {
 	
 }
 
-//Map is a 5x5 table element
+//Map is a 10x10 table element
 //Declare keys for items
-var mapSize = 5;
+var mapSize = 10;
 var mapID_field = ".";
-var mapID_woods = "f";
-var mapID_hill = "^";
+var mapID_woods = ",";
+var mapID_mountains = "^";
+var mapID_water = "~";
 var mapID_mech = "M";
 var mapElement = document.getElementById("map");
 
@@ -31,6 +32,10 @@ function buildMap() {
 	
 	let result = [];
 	
+	let num_woods = 2;
+	let num_mountains = 1;
+	let num_rivers = 1;
+	
 	for (var y = 0; y < mapSize; y++) {
 		
 		let row = [];
@@ -39,12 +44,6 @@ function buildMap() {
 			
 			var tile = mapID_field;
 			
-			if (Math.random() > .8) {
-				
-				tile = mapID_woods;
-				
-			}
-			
 			row.push(tile);
 			
 		}
@@ -52,7 +51,46 @@ function buildMap() {
 		result.push(row);
 	}
 	
+	//Add blobs of woodland
+	
+	result = addBlob(mapID_woods, num_woods, 1, result);
+	
+	//Add blobs of mountains
+	
+	result = addBlob(mapID_mountains, num_mountains, 1, result);
+	
+	//Run rivers across map
+	
+	for (var i = 0; i < num_rivers; i++) {
+		let riverY = 2 + Math.floor(Math.random() * mapSize-2);
+		for (var x = 0; x < mapSize; x++) {
+			result[riverY][x] = mapID_water;
+		}
+	}
+	
 	return result;
+	
+}
+
+function addBlob(entity, amount, spread, mapObject) {
+	
+	for (var i = 0; i < amount; i++) {
+		let posX = Math.floor(Math.random() * mapSize);
+		let posY = Math.floor(Math.random() * mapSize);
+		
+		
+		mapObject[posX][posY] = entity;
+		
+		if (spread > 0) {
+		
+			if (posX > 0) {mapObject[posX-1][posY] = entity};
+			if (posX < mapSize-1) {mapObject[posX+1][posY] = entity};
+			if (posY > 0) {mapObject[posX][posY-1] = entity};
+			if (posY < mapSize-1) {mapObject[posX][posY+1] = entity};
+		}
+	}
+	
+	return mapObject;
 	
 }
 
@@ -73,6 +111,8 @@ function updateMap() {
 		
 		//Iterate over cells
 		for (var j = 0; j < cells.length; j++) {
+			
+			//i = y; j = x
 			
 			//For each cell, set the text content to be the map tile
 			cells[j].textContent = map[i][j];
